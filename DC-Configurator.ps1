f3d1840ca2bb888f32aed45e67a6cd055f8c2e9f
Haydz <haydnit@gmail.com>
2020-09-06 15:15:44 -0400

    Added PowerShell Logging
    Ensure that powershell is logging despite GPO config

--- DC-Configurator.ps1
+++ DC-Configurator.ps1
@@ -47,6 +47,20 @@ foreach ($file in (Get-ChildItem *.xml)) {wecutil cs $file}
 Wevtutil gl WEC3-PRINT
 foreach ($subscription in (wevtutil el | select-string -pattern "WEC")) {wevtutil sl $subscription /ms:4194304}
 Wevtutil gl WEC3-PRINT
+#add full auditpol.exe
+auditpol.exe /set /category:* /sucess:enable
+auditpol.exe /set /category:* /failure:enable
+#Enable PowerShell ScriptBlock logging & Module Logging
+#Add PowerShell folder for policies   
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "PowerShell"
+#EnablePowerShell ScriptBlockLogging
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell" -Name "ScriptBlockLogging"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" -Name "EnableScriptBlockLogging " -Value 1 -PropertyType "DWORD"
+#Add PowerShell Module logging, set to ALL
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell" -Name "ModuleLogging"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" -Name "EnableModuleLogging" -Value 1 -PropertyType "DWORD"
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" -Name "ModuleNames"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames" -Name "*" -Value "*" -PropertyType "String"
 gpupdate /force
 mv C:\labs\WinLogBeat\winlogbeat-7.7.1-windows-x86_64\winlogbeat.yml C:\labs\WinLogBeat\winlogbeat-7.7.1-windows-x86_64\winlogbeat.yml.old
 cp C:\labs\LabPack\LABPACK-master\Lab-WinLogBeat\winlogbeat.yml C:\labs\WinLogBeat\winlogbeat-7.7.1-windows-x86_64\winlogbeat.yml
--- WS-Configurator.ps1
+++ WS-Configurator.ps1
@@ -1,5 +1,20 @@
 cd \\dc01\labs\sysmon\
 ./sysmon64.exe -accepteula -i sysmonconfig.xml
 Get-WinEvent -LogName Microsoft-Windows-Sysmon/Operational
+#add full auditpol.exe
+auditpol.exe /set /category:* /sucess:enable
+auditpol.exe /set /category:* /failure:enable
+Set-Itemproperty -path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" -Value 1
+#Enable PowerShell ScriptBlock logging & Module Logging
+#Add PowerShell folder for policies   
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "PowerShell"
+#EnablePowerShell ScriptBlockLogging
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell" -Name "ScriptBlockLogging"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" -Name "EnableScriptBlockLogging " -Value 1 -PropertyType "DWORD"
+#Add PowerShell Module logging, set to ALL
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell" -Name "ModuleLogging"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" -Name "EnableModuleLogging" -Value 1 -PropertyType "DWORD"
+New-Item –Path  "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" -Name "ModuleNames"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames" -Name "*" -Value "*" -PropertyType "String"
 gpupdate /force
 Restart-Computer
